0a530eb0cec531f27cf4b647df0a8453e4b01963
lupin <steffenlorenz@hotmail.de>
2025-06-07 11:20:21 +0200

    add generic urls for download pages

--- config/config.py
+++ config/config.py
@@ -1,22 +1,27 @@
 CONFIGURED_DEVICES: dict = {
     "ESP32": {
         "name": "esp32",
-        "write_flash": 0x1000
+        "write_flash": 0x1000,
+        "url": "https://micropython.org/download/ESP32_GENERIC/"
     },
     "ESP32-S2": {
         "name": "esp32s2",
-        "write_flash": 0x1000
+        "write_flash": 0x1000,
+        "url": "https://micropython.org/download/ESP32_GENERIC_S2/"
     },
     "ESP32-S3": {
         "name": "esp32s3",
-        "write_flash": 0
+        "write_flash": 0,
+        "url": "https://micropython.org/download/ESP32_GENERIC_S3/"
     },
     "ESP32-C3": {
         "name": "esp32c3",
-        "write_flash": 0
+        "write_flash": 0,
+        "url": "https://micropython.org/download/ESP32_GENERIC_C3/"
     },
     "ESP32-C6": {
         "name": "esp32c6",
-        "write_flash": 0
+        "write_flash": 0,
+        "url": "https://micropython.org/download/ESP32_GENERIC_C6/"
     }
 }
