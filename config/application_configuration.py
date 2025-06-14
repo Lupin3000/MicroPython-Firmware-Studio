8add3fa5f17a77bad20927cf56845547e7a1a23c
lupin <steffenlorenz@hotmail.de>
2025-06-14 20:58:15 +0200

    restructure configuration

--- config/application_configuration.py
+++ config/application_configuration.py
@@ -1,4 +1,9 @@
-# colors
+# application settings
+TITLE: str = 'MicroPython Firmware Studio'
+LOG_LEVEL: str = 'INFO'
+VERIFICATION_MODE: bool = True
+
+# application colors
 APPEARANCE_MODE: str = 'dark'
 COLOR_THEME: str = 'blue'
 CONSOLE_INFO: str = "green2"
@@ -6,17 +11,10 @@ CONSOLE_COMMAND: str = "white"
 CONSOLE_ERROR: str = "OrangeRed2"
 LINK_OBJECT: str = "dodger blue"
 
-# title
-TITLE: str = 'MicroPython Firmware Studio'
-
-# fonts
+# application fonts
 FONT_PATH: tuple = ('Arial', 20, 'bold')
 FONT_CATEGORY: tuple = ('Arial', 16, 'bold')
 FONT_DESCRIPTION: tuple = ('Arial', 14)
 
 # images
 RELOAD_ICON: str = 'img/reload.png'
-
-# settings
-LOG_LEVEL: str = 'INFO'
-VERIFICATION_MODE: bool = True
