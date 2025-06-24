040a836cd2b2dbc5f4ac83cf7f60512d20c2f7ed
lupin <steffenlorenz@hotmail.de>
2025-06-24 12:38:37 +0200

    finsh serial refactoring

--- serial_plugin/__init__.py
+++ serial_plugin/__init__.py
@@ -1,6 +1,3 @@
-from .serial_base import SerialBase
 from .serial_command_runner import SerialCommandRunner
-from .serial_get_file_structure import FileStructure
-from .serial_get_version import Version
 
 __all__ = ["SerialCommandRunner"]
