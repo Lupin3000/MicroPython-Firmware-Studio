991f5006ffa1f02a3fb0bd40c362493b1b4b8144
lupin <steffenlorenz@hotmail.de>
2025-06-24 13:43:12 +0200

    fine-tuning of changes for serial

--- serial_plugin/__init__.py
+++ serial_plugin/__init__.py
@@ -1,3 +1,10 @@
+from .serial_base import SerialBase
 from .serial_command_runner import SerialCommandRunner
+from .serial_get_file_structure import FileStructure
+from .serial_get_version import Version
 
-__all__ = ["SerialCommandRunner"]
+
+__all__ = ["SerialBase",
+           "SerialCommandRunner",
+           "FileStructure",
+           "Version"]
